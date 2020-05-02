26f6f088594239dc3eba92dd187f137e78a00018
joe1537 <j808312@gmail.com>
2020-05-02 10:30:02 +0900

    最初のコミット

---
+++ wordpress.js
@@ -0,0 +1,8 @@
+jQuery(function($){
+    $('.tab').on('click',function(){
+      var idx=$('.tab').index(this);
+      $(this).addClass('is-active').siblings('.tab').removeClass('is-active');
+      $(this).closest('.tab-group').next('.panel-group').find('.panel').removeClass('is-show');
+      $('.panel').eq(idx).addClass('is-show');
+    });
+  });
